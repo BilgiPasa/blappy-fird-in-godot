541b7d7cba517a36b75672bfadc53e89432327ce
BilgiPasa <bilgicaglarbozkurt@gmail.com>
2026-01-31 13:22:02 +0300

    Updating the Pause Menu

--- Scripts/pause_menu.gd
+++ Scripts/pause_menu.gd
@@ -5,11 +5,15 @@ signal quit_game
 
 @export var resume_button: Button
 
-# Buraya "_on_resume_button_pressed" ve "_on_quit_game_button_pressed" fonksiyonlarını ekle.
-
 func _on_visibility_changed() -> void:
 	if visible:
 		resume_button.grab_focus()
+
+func _on_resume_button_pressed() -> void:
+	emit_signal("resume");
+
+func _on_quit_game_button_pressed() -> void:
+	emit_signal("quit_game");
 
 """ Bu arada haberin olsun, UI navigation'da WASD kullanabiliyorum çünkü; "Project Settings"te "Input Map"te
 "Show Built-in Actions" seçeneğini aktive ettim ve ui_up, ui_down, ui_right ve ui_left kısımlarına WASD
